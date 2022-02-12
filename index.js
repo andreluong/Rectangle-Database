428f16a400048b27795b6463f345727647402900
Andre Luong <andreluong1@gmail.com>
2022-02-11 22:06:27 -0800

    test

--- index.js
+++ index.js
@@ -3,8 +3,7 @@ const path = require('path')
 const PORT = process.env.PORT || 5000
 
 const { Pool } = require('pg');
-var pool;
-pool = new Pool({
+var pool = new Pool({
   connectionString: process.env.DATABASE_URL
 });
 
@@ -12,20 +11,22 @@ var app = express();
 app.use(express.json());
 app.use(express.urlencoded({extended:false}));
 app.use(express.static(path.join(__dirname, 'public')));
+
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
 app.get('/', (req, res) => res.render('pages/index'));
+
 app.get('/database', (req,res) => {
-  var getUsersQuery = `SELECT * FROM rectangle`;
+  var getUsersQuery = `SELECT * FROM rectangles`;
   pool.query(getUsersQuery, (error,result) => {
     if (error) 
       res.end(error);
     var results = {'rows':result.rows};
     res.render('pages/db',results);
   });
-
 });
+
 // app.post('/adduser', (req,res) => {
 //   console.log("post request for /adduser");
 //   var username = req.body.username;
