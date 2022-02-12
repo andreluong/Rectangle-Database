0848208d8e12a3ff5d1d7554cfac1083fbb54374
Andre Luong <andreluong1@gmail.com>
2022-02-12 13:58:18 -0800

    c

--- index.js
+++ index.js
@@ -5,44 +5,32 @@ const PORT = process.env.PORT || 5000
 const { Pool } = require('pg');
 var pool;
 pool = new Pool({
-  connectionString: process.env.DATABASE_URL
+  connectionString: process.env.DATABASE_URL || 'postgres://postgres:824807@localhost/usr'
 })
 
 var app = express();
 app.use(express.json());
 app.use(express.urlencoded({extended:false}));
-app.use(express.static(path.join(__dirname, 'public')));
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
+app.use(express.static(path.join(__dirname, 'public')))
+app.set('views', path.join(__dirname, 'views'))
+app.set('view engine', 'ejs')
 
-app.get('/', (req, res) => res.render('pages/index'));
+app.get('/', (req, res) => res.render('pages/index'))
 
 app.get('/database', (req,res) => {
-  // var getUsersQuery = `SELECT * FROM rectangle;`;
-  // pool.query(getUsersQuery, (error,result) => {
-  //   if (error) {
-  //     res.end(error);
-  //   }
-  //   var results = {'rows':result.rows}
-  //   // data = {results : result.rows};
-  //   res.render('pages/db',results); 
-  // })
-  res.render('pages/db'); 
-});
-
-// app.post('/adduser', (req,res) => {
-//   console.log("post request for /adduser");
-//   var username = req.body.username;
-//   var age = req.body.age;
-//   res.send(`username: ${username}, age: ${age}`);
-// });
-// app.get('/users/:id', (req,res) => {
-//   var userId = req.params.id;
-//   console.log(req.params.id);
-//   // search database using userId
-//   res.send("got user id!");
-// })
+  // var data = {results: [1,2,3,4,5]};
+  var getUserQuery = `SELECT * FROM usr`;
+  pool.query(getUserQuery, (error, result) => {
+    if (error)
+      res.end(error)
+    var results = {'rows':result.rows}
+    res.render('pages/db', results);
+  }) 
+  // res.render('pages/db', data);
+})
 
 
-app.listen(PORT, () => console.log(`Listening on ${ PORT }`));
+
+app.listen(PORT, () => console.log(`Listening on ${ PORT }`))
+
 
