dd9f53580dd4a2be4cc0a62c2488a6272d99eb51
Andre Luong <andreluong1@gmail.com>
2022-02-11 18:29:38 -0800

    database recheck fix

--- index.js
+++ index.js
@@ -14,17 +14,17 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
-// app.get('/', (req, res) => res.render('pages/index'));
-// app.get('/database', (req,res) => {
-//   var getUsersQuery = `SELECT * FROM rectangle`;
-//   pool.query(getUsersQuery, (error,result) => {
-//     if (error) 
-//       res.end(error);
-//     var results = {'rows':result.rows};
-//     res.render('pages/db',results);
-//   });
+app.get('/', (req, res) => res.render('pages/index'));
+app.get('/database', (req,res) => {
+  var getUsersQuery = `SELECT * FROM rectangle`;
+  pool.query(getUsersQuery, (error,result) => {
+    if (error) 
+      res.end(error);
+    var results = {'rows':result.rows};
+    res.render('pages/db',results);
+  });
 
-// });
+});
 // app.post('/adduser', (req,res) => {
 //   console.log("post request for /adduser");
 //   var username = req.body.username;
