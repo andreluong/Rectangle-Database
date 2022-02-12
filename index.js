b1a0a0b6eabaabcb67f359fe7a06ca94ffae91fa
Andre Luong <andreluong1@gmail.com>
2022-02-12 14:16:12 -0800

    y

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const PORT = process.env.PORT || 5000
 const { Pool } = require('pg');
 var pool;
 pool = new Pool({
-  connectionString: process.env.DATABASE_URL || 'postgres://postgres:824807@localhost/usr'
+  connectionString: process.env.DATABASE_URL || 'postgres://postgres:824807@localhost/users'
 })
 
 var app = express();
