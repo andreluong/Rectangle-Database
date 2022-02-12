94ffb8f9819cda9fce0a12351c04c1234e92747f
Andre Luong <andreluong1@gmail.com>
2022-02-11 22:55:22 -0800

    testing

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ app.get('/database', (req,res) => {
   pool.query(getUsersQuery, (error,result) => {
     if (error) 
       res.end(error);
-    var results = {'rows':result.rows};
+    // var results = {'rows':result.rows};
     res.render('pages/db',results); 
   });
 
