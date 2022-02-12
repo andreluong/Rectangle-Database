3aed3e96321de3a3e51103eee6b475b9d9df2e6d
Andre Luong <andreluong1@gmail.com>
2022-02-12 14:22:25 -0800

    p

--- index.js
+++ index.js
@@ -2,35 +2,43 @@ const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 5000
 
-const { Pool } = require('pg');
-var pool;
-pool = new Pool({
-  connectionString: process.env.DATABASE_URL || 'postgres://postgres:824807@localhost/users'
-})
+var app = express()
 
-var app = express();
-app.use(express.json());
-app.use(express.urlencoded({extended:false}));
+const { Pool } = require('pg');
+var pool = new Pool({
+    connectionString: process.env.DATABASE_URL || "postgres://postgres:824807@localhost/users"
+})
+  
 app.use(express.static(path.join(__dirname, 'public')))
+app.use(express.json())
+app.use(express.urlencoded({extended:false}))
+
 app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'ejs')
-
 app.get('/', (req, res) => res.render('pages/index'))
-
-app.get('/database', (req,res) => {
-  // var data = {results: [1,2,3,4,5]};
-  var getUserQuery = `SELECT * FROM usr`;
-  pool.query(getUserQuery, (error, result) => {
-    if (error)
-      res.end(error)
-    var results = {'rows':result.rows}
-    res.render('pages/db', results);
-  }) 
-  // res.render('pages/db', data);
+app.get('/database', async (req,res)=>{
+   try {
+       const result = await pool.query(`SELECT * FROM usr`);
+       const data = { results : result.rows };
+       res.render('pages/db', data);
+   }
+   catch (error) {
+        res.end(error);
+   }
+    
 })
-
-
+// app.post('/login', (req,res)=>{
+//     console.log("post to /login")
+//     console.log(req.body)
+//     let un = req.body.uname;
+//     let pw = req.body.pass;
+//     // database?
+//     userPasswordQuery = `SELECT * FROM users WHERE name = '${un}'`;
+//     res.send("got it!")
+// })
+// app.get('/t/:id', (req,res)=>{
+//     console.log(req.params.id);
+//     res.send(`ID: ${req.params.id}`);
+// })
 
 app.listen(PORT, () => console.log(`Listening on ${ PORT }`))
-
-
