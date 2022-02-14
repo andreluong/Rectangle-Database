2e9690de3c82ce02c75076c146647f29d721733f
Andre Luong <andreluong1@gmail.com>
2022-02-14 09:54:09 -0800

    changed /add

--- index.js
+++ index.js
@@ -29,16 +29,27 @@ app.get('/database', async (req, res) => {
       res.render('pages/db', results );
       client.release();
     } catch (err) {
-      console.error(err);
       res.send("Error " + err);
     }
   })
-
-
     
 app.get('/rectangle', (req,res) => res.render('pages/rectangle'))
 
-app.get('/add', (req,res) => res.render('pages/add'))
+app.get('/add', (req,res) => {
+    var nameValue = document.querySelector("name").value;
+    var widthValue = document.querySelector("width").value;
+    var heightValue = document.querySelector("height").value;
+    var colourValue = document.querySelector("colour").value;
+
+    const getAddQuery = `insert into rect values (${nameValue},${widthValue},${heightValue},${colourValue})`
+    pool.query(getAddQuery, (error,result) => {
+        if (error) {
+            res.send("Error " + err);
+        }
+        res.render('pages/db');
+    })
+
+})
 
 
 app.listen(PORT, () => console.log(`Listening on ${ PORT }`))
