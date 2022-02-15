a58c089a7f8721cc90f5fc4e4d83593a73f06aeb
Andre Luong <andreluong1@gmail.com>
2022-02-15 14:15:48 -0800

    changing rectangle page

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ app.get('/', (req, res) => res.render('pages/index'))
 app.get('/database', async (req, res) => {
     try {
       const client = await pool.connect();
-      const result = await client.query('SELECT * from rect');
+      const result = await client.query('select * from rect');
       const results = { 'results': (result) ? result.rows : null};
       res.render('pages/db', results);
       client.release();
@@ -33,7 +33,9 @@ app.get('/database', async (req, res) => {
 })
 app.get('/rectangle', (req,res) => res.render('pages/rectangle'))
 app.get('/database/:name', (req,res) => {
-  res.render('pages/rectangle', {name: req.params.name});
+  const client = await pool.connect();
+  const result = client.query(`select * from rect where name='${req.params.name}`);
+  res.render('pages/rectangle', result);
 })
 app.get('/add', (req,res) => res.render('pages/add'))
 
@@ -45,7 +47,7 @@ app.post('/add', async (req,res) => {
 
   try {
     const client = await pool.connect();
-    client.query(`INSERT INTO rect VALUES('${name}',${width},${height},'${colour}')`);
+    client.query(`insert into rect values ('${name}',${width},${height},'${colour}')`);
     res.redirect('/database');
   } catch (err) {
     res.send("Error " + err);
